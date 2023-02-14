2bdf616152804c770c06138f11e73fc953bb2cb4
Davi Lucciola <davilucciola14@gmail.com>
2023-02-13 22:02:46 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Treino HTML e CSS Vanila
+
+Fazer uma Pagina de Pizzaria
---
+++ js/main.js
@@ -0,0 +1,9 @@
+let PAGES = ['Sabores', 'Pedido'];
+let page = 'Sabores';
+
+switch (page.toLowerCase()) {
+    case 'sabores':
+        break;
+    case 'pedido':
+        break
+}
